97a41024ba5a397071757e3b09f554027541873b
bilel <mbchriki@gmail.com>
2024-12-06 16:30:08 +0100

    enum + fichier

--- src/Metier/DifficulteQuestion.java
+++ src/Metier/DifficulteQuestion.java
@@ -2,8 +2,12 @@ package Metier;
 
 public enum DifficulteQuestion 
 {
-	TRESFACILE(),
-	FACILE	  (),
-	MOYEN	  (),
-	DIFFICILE ();
+	TRESFACILE("#82CE84"),
+	FACILE	  ("#737EC9"),
+	MOYEN	  ("#E6415B"),
+	DIFFICILE ("#827D7C");
+
+	private String libelle;
+
+	DifficulteQuestion(String lib){this.libelle = lib;}
 }
