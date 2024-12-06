8fca7400fb1ed51795efc4a3a1b044d02b8609d8
bilel <mbchriki@gmail.com>
2024-12-06 16:50:02 +0100

    ressource notion evalutation

--- src/Metier/Evalutation.java
+++ src/Metier/Evalutation.java
@@ -1,5 +1,16 @@
 package Metier;
 
-public class Evalutation {
-	
+public class Evalutation 
+{
+	private int nbQuestions;
+	private boolean evaluation;
+
+	public Evalutation(int nb, boolean eval)
+	{
+		this.nbQuestions = nb;
+		this.evaluation  = eval;
+	}
+
+	public boolean isEvaluee (){return this.evaluation;}
+	public int getNbQuestions(){return this.nbQuestions;}
 }
