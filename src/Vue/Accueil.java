b4a710d9115126afd365653cc115c00444eca2c0
Evan Cnaepelnickx <evan.cnaepelnickx@etu.univ-lehavre.fr>
2024-12-09 09:55:05 +0100

    ajout du titre

--- src/Vue/Accueil.java
+++ src/Vue/Accueil.java
@@ -19,31 +19,30 @@ public class Accueil extends JFrame {
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 
-		// Création du panel avec GridBagLayout pour centrer les boutons
 		pnlBtns = new JPanel(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.insets = new Insets(10, 10, 10, 10); // marges entre les boutons
+		gbc.insets = new Insets(10, 10, 10, 10);
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		gbc.gridx = 0;
-		gbc.gridy = GridBagConstraints.RELATIVE; // pour empiler les boutons verticalement
+		gbc.gridy = GridBagConstraints.RELATIVE;
 
-		// Création des boutons de même taille
+		lblTitre = new JLabel("QCM Builder");
+		lblTitre.setFont(new Font("Arial", Font.BOLD, 30));
+
 		btnCreerQuestion = new JButton("Créer une question");
 		btnCreerEval = new JButton("Créer une évaluation");
 		btnParametres = new JButton("Paramètres");
 
-		// Définir la même taille pour tous les boutons
 		Dimension btnSize = new Dimension(200, 50);
 		btnCreerQuestion.setPreferredSize(btnSize);
 		btnCreerEval.setPreferredSize(btnSize);
 		btnParametres.setPreferredSize(btnSize);
 
-		// Ajout des boutons au panel avec les contraintes
+		pnlBtns.add(lblTitre);
 		pnlBtns.add(btnCreerQuestion, gbc);
 		pnlBtns.add(btnCreerEval, gbc);
 		pnlBtns.add(btnParametres, gbc);
 
-		// Ajout du panel à la fenêtre
 		this.add(pnlBtns);
 
 		this.setVisible(true);
